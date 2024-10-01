cd03a42e300a11105b7edc39aef861ab840fabaa
Kaustubh <kaustubhkadam147@gmail.com>
2024-10-02 00:56:00 +0530

    Fixed lots of bugs related to migration

--- startup_manager/startup_manager/startup_manager/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# def home(request):
-#     # return HttpResponse("Hello, World. You are at chai aur Django Home page")
-#     return render(request, 'homepage.html')
