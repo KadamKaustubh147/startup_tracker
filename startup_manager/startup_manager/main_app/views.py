785e5e5265ea57357d07a6d0dbc2710f1eb1f3ca
Kaustubh <kaustubhkadam147@gmail.com>
2024-10-02 10:08:00 +0530

    Removed startup manager and made changes in backend

--- startup_manager/startup_manager/main_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(request):
-    # return HttpResponse("Hello, World. You are at chai aur Django Home page")
-    return render(request, 'homepage.html')
